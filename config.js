a50d8d23c91d51e16bd01a04015ca7b94db515c6
RebeccaOlatunde <54245689+RebeccaOlatunde@users.noreply.github.com>
2021-07-11 14:22:20 -0400

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '316947216604612',
-        clientSecret: 'ebc9eaad53e16f93ee6bdacb672bed8a'
-    }
-}
